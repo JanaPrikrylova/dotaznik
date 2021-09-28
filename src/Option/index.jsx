d1a4c07f86cd511e6dea0a81b7a449c9d9be2c89
JanaPrikrylova <prikrylovajana003@gmail.com>
2021-09-28 13:46:41 +0200

    React - communication between components

--- src/Option/index.jsx
+++ src/Option/index.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import Icon from '../Icon/index';
 import './style.css';
 
-const Option = (props) => (
-  <div className="option">
-    <Icon type={props.type} />
-    <div>{props.text}</div>
+const Option = ({type, text, onSelect}) => {
+  const handleClick = () => onSelect(type);
+
+  return (
+  <div onClick={handleClick} className="option">
+    <Icon type={type} />
+    <div>{text}</div>
   </div>
 );
+}
 
 export default Option;
--- src/Question/index.jsx
+++ src/Question/index.jsx
@@ -1,22 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import QuestionBody from '../QuestionBody/index';
 import Option from '../Option/index';
 import './style.css';
 
-const Question = (props) => (
-  <div className="question">
-    <QuestionBody 
-      iconType="symbolQuestion" 
-      text={props.text}
-    />
-    <div className="question__options">
-      <Option type="smileyStrongYes" text="Souhlasím"/>
-      <Option type="smileyYes" text="Spíše souhlasím"/>
-      <Option type="smileyNeutral" text="Nevím"/>
-      <Option type="smileyNo" text="Spíše nesouhlasím"/>
-      <Option type="smileyStrongNo" text="Nesouhlasím"/>
+const Question = ({text}) => {
+  // const [answer, setAnswered] = useState(false);
+  const [answer, setAnswered] = useState('symbolQuestion');
+
+  const handleSelect = (iconType) => setAnswered(iconType);
+
+
+    return (
+    <div className="question">
+      <QuestionBody 
+        iconType={answer} 
+        // iconType={answered === true ? 'symbolTick' : 'symbolQuestion'} 
+        text="Kdo jinému jámu kopá, sám do ní padá"
+      />
+      <div className="question__options">
+        <Option 
+          onSelect={handleSelect}
+          type="smileyStrongYes" 
+          text="Souhlasím"
+        />
+        <Option 
+          onSelect={handleSelect}
+          type="smileyYes" 
+          text="Spíše souhlasím"
+        />
+        <Option 
+          onSelect={handleSelect}
+          type="smileyNeutral" 
+          text="Nevím"
+        />
+        <Option 
+          onSelect={handleSelect}
+          type="smileyNo" 
+          text="Spíše nesouhlasím"
+        />
+        <Option 
+          onSelect={handleSelect}
+          type="smileyStrongNo" 
+          text="Nesouhlasím"
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+}
 
 export default Question;
